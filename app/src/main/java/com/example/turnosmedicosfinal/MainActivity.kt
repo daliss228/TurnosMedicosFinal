763590e131c113c80683aea3b63eae8435d95a2c
daliss228 <dayanalissrodriguez@gmail.com>
2025-08-02 20:48:41 -0500

    conexión con Firestore

--- app/src/main/java/com/example/turnosmedicosfinal/MainActivity.kt
+++ app/src/main/java/com/example/turnosmedicosfinal/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import com.google.firebase.FirebaseApp
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
